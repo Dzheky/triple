a1656c1ce900445cb09c6168fa6586f0053a8e76
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2017-05-16 19:46:02 +0300

    added timer

--- App/Components/EventBox.js
+++ App/Components/EventBox.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import styles from './Styles/EventBoxStyle'
+import favoriteStyles from './Styles/EventBoxFavoriteStyle'
 import EventActions from '../Redux/EventsRedux'
 import { connect } from 'react-redux'
 import { Colors, Metrics } from '../Themes/'
---
+++ App/Components/Styles/EventBoxFavoriteStyle.js
@@ -0,0 +1,120 @@
+import { StyleSheet } from 'react-native'
+import { Metrics, Colors, Fonts } from '../../Themes/'
+
+export default StyleSheet.create({
+  row: {
+    flex: 1,
+    backgroundColor: Colors.coal,
+    marginVertical: Metrics.smallMargin,
+    marginHorizontal: Metrics.baseMargin,
+    justifyContent: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.3
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 0,
+    top: 0,
+    width: Metrics.screenWidth - Metrics.baseMargin * 2,
+    height: 150,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    position: 'absolute',
+    zIndex: 99
+  },
+  dots: {
+    color: Colors.coal,
+    fontSize: Fonts.size.regular,
+    fontWeight: 'bold',
+    backgroundColor: Colors.transparent
+  },
+  numberSetContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberSetTitle: {
+    fontSize: Fonts.size.small,
+    fontWeight: 'bold',
+    color: Colors.coal
+  },
+  numberSet: {
+    flexDirection: 'row',
+  },
+  numberContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.coal,
+    paddingVertical: Metrics.smallMargin,
+    width: Metrics.doubleBaseMargin,
+    margin: 1
+  },
+  numberMiddleLine: {
+    position: 'absolute',
+    width: Metrics.doubleBaseMargin,
+    height: 1,
+    backgroundColor: Colors.snow,
+  },
+  number: {
+    fontWeight: 'bold',
+    fontSize: Fonts.size.h6,
+    color: Colors.yellow,
+  },
+  likeContainer: {
+    position: 'absolute',
+    top: 130,
+    right: Metrics.baseMargin,
+    zIndex: 100,
+    height: 40,
+    width: 40,
+    borderRadius: 100,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  likeIcon: {
+    backgroundColor: Colors.transparent,
+    height: 27
+  },
+  boldLabel: {
+    ...Fonts.style.h5,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    color: Colors.yellow,
+    textAlign: 'left',
+    width: Metrics.screenWidth - 80,
+    marginTop: Metrics.smallMargin,
+    marginHorizontal: Metrics.baseMargin
+  },
+  underLabel: {
+    color: Colors.yellow,
+    fontSize: Fonts.size.small,
+    marginHorizontal: Metrics.baseMargin
+  },
+  greyLine: {
+    height: 1,
+    backgroundColor: Colors.yellow,
+    opacity: 0.2,
+    marginHorizontal: Metrics.baseMargin,
+    marginVertical: Metrics.smallMargin
+  },
+  bottomContainer: {
+    marginHorizontal: Metrics.baseMargin,
+    marginBottom: Metrics.smallMargin,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  dates: {
+    fontSize: Fonts.size.small,
+    color: Colors.yellow
+  },
+  price: {
+    fontSize: Fonts.size.small,
+    color: Colors.yellow
+  },
+  image: {
+    width: Metrics.screenWidth - Metrics.baseMargin * 2,
+    height: 150
+  }
+})
