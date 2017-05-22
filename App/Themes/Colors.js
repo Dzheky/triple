9310a779c1a39fc9f9e67238f512750af828bc7c
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2017-05-22 11:03:53 +0300

    fixed colors file

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -24,35 +24,7 @@ const colors = {
   eggplant: '#251a34',
   border: '#483F53',
   banner: '#5F3E63',
-  text: '#E0D7E5',
-  getMonth: (month) => {
-      switch (month) {
-        case '01':
-          return 'Января'
-        case '02':
-          return 'Февраля'
-        case '03':
-          return 'Марта'
-        case '04':
-          return 'Апреля'
-        case '05':
-          return 'Мая'
-        case '06':
-          return 'Июня'
-        case '07':
-          return 'Июля'
-        case '08':
-          return 'Августа'
-        case '09':
-          return 'Сентября'
-        case '10':
-          return 'Октября'
-        case '11':
-          return 'Ноября'
-        default:
-          return 'Декабря'
-      }
-  }
+  text: '#E0D7E5'
 }
 
 export default colors
